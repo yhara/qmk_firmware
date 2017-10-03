a495960989edd7614f376f9685714f7693370757
Yutaka HARA <yutaka.hara+github@gmail.com>
2017-10-03 18:53:15 +0900

    Remove number keys!
    to be prepared for Let's Split
    
    - Remove number keys
    - Remove BS from LOWER, RAISE (use the one on the base layer)

--- keyboards/ergodox_ez/keymaps/yhara/keymap.c
+++ keyboards/ergodox_ez/keymaps/yhara/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_BSLS,        KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
+        KC_NO,          KC_NO,        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_GRV,
         CTL_T(KC_ESC),  KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_EQL,
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                 KC_HOME,
                                                 KC_SPC,KC_BSLS, KC_N,
         // right hand
-             X(0),        KC_6,   KC_7,    KC_8,   KC_9,   KC_0,             KC_MINS,
+             X(0),        KC_NO,  KC_NO,   KC_NO,  KC_NO,  KC_NO,            KC_NO,
              KC_QUOT,     KC_Y,   KC_U,    KC_I,   KC_O,   KC_P,             KC_BSPC,
                           KC_H,   KC_J,    KC_K,   KC_L,   KC_SCLN,          KC_ENT,
              KC_NO,       KC_N,   KC_M,    KC_COMM,KC_DOT, KC_SLSH,          SFT_T(KC_MINS),
@@ -146,9 +146,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   !  |  @   |   #  |  $   |  %   |      |           |      |  ^   |  &   |   (  |  )   |  *   |   -    |
+ * |        |   !  |  @   |   #  |  $   |  %   |      |           |      |  ^   |  &   |   (  |  )   |  *   |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   ~  |  |   |   =  |  \   |  `   |------|           |------|  BS  |  "   |  [   |   ]  |  '   |        |
+ * |        |   ~  |  |   |   =  |  \   |  `   |------|           |------|      |  "   |  [   |   ]  |  '   |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |  +   |  {   |   }  |  _   |   _    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -173,9 +173,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_NO  , KC_NO  , KC_NO  ,
     // right hand
        KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
-       KC_NO  ,  KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_ASTR, KC_MINS,
-                 KC_BSPC, KC_DQT , KC_LBRC, KC_RBRC, KC_QUOT, KC_NO  ,
-       KC_NO  ,  KC_NO  , KC_PLUS, KC_LCBR, KC_RCBR, KC_UNDS, KC_UNDS,
+       KC_NO  ,  KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_ASTR, KC_TRNS,
+                 KC_NO  , KC_DQT , KC_LBRC, KC_RBRC, KC_QUOT, KC_TRNS,
+       KC_NO  ,  KC_NO  , KC_PLUS, KC_LCBR, KC_RCBR, KC_UNDS, KC_TRNS,
                           KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
        KC_NO  , KC_NO  ,
        KC_NO  ,
@@ -188,7 +188,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |   1  |  2   |   3  |  4   |  5   |      |           |      |  6   |  7   |  8   |   9  |  0   |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        | Home |      |  Del |      | End  |------|           |------| BS   | Down |  Up  | Right|      |        |
+ * |        | Home |      |  Del |      | End  |------|           |------|      | Down |  Up  | Right|      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  | F10  |  F11   |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -213,8 +213,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_NO  , KC_NO  , KC_NO  ,
     // right hand
        KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
-       KC_NO  ,  KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
-                 KC_BSPC, KC_DOWN, KC_UP  , KC_RIGHT,KC_NO  , KC_NO,
+       KC_NO  ,  KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_TRNS,
+                 KC_NO  , KC_DOWN, KC_UP  , KC_RIGHT,KC_NO  , KC_NO,
        KC_NO  ,  KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11,
                           KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_F12  ,
        KC_NO  , KC_NO  ,
