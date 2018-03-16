5cdf836a52b65f9fcd023962aef8407158291f1b
Yutaka HARA <yutaka.hara+github@gmail.com>
2018-03-16 20:43:25 +0900

    Fixed!
    When using LT(_LOWER, *) or MO(_LOWER, *), the key to invoke LOWER
    should be mapped to KC_TRNS instead of KC_NO in the LOWER layer mapping.

--- keyboards/lets_split/keymaps/yhara/keymap.c
+++ keyboards/lets_split/keymaps/yhara/keymap.c
@@ -19,17 +19,17 @@ extern keymap_config_t keymap_config;
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
-//// Macros
-//const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-//{
-//  // MACRODOWN only works in this function
-//      switch(id) {
-//        case 0:
-//          SEND_STRING(SS_LCTRL("z")"[");
-//          break;
-//      }
-//    return MACRO_NONE;
-//};
+// Macros
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+  // MACRODOWN only works in this function
+      switch(id) {
+        case 0:
+          SEND_STRING(SS_LCTRL("z")"[");
+          break;
+      }
+    return MACRO_NONE;
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -48,43 +48,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
 CTL_T(KC_ESC),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT(_MEDIA, KC_ENT), \
   KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS , \
-  LT(_ADJUST,KC_LEFT),ALT_T(KC_DOWN),KC_A,GUI_T(KC_RIGHT),LT(_LOWER,KC_LANG2),KC_SPC, LT(_CURSOR,KC_SPC),LT(_RAISE,KC_LANG1),KC_LEFT,KC_DOWN,KC_UP,KC_RGHT \
+  LT(_ADJUST,KC_LEFT),ALT_T(KC_DOWN),LT(_CURSOR,KC_UP),GUI_T(KC_RIGHT),LT(_LOWER,KC_LANG2),KC_SPC, LT(_CURSOR,KC_SPC),LT(_RAISE,KC_LANG1),KC_LEFT,KC_DOWN,KC_UP,KC_RGHT \
 ),
 
 /* CURSOR
  * ,-----------------------------------------.    ,------------------------------------------.
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |    |  F6  |  F7  |  F8  |  F9  |  F10 |  F11  |
- * |------+------+------+------+------+------|    |------+------+------+------+------+-------|
  * |      |      |      | End  |      |      |    |      |      |      |      |      | BkSp  |
  * |------+------+------+------+------+------|    |------+------+------+------+------+-------|
- * |      | Home |      | Del  |PgDn  |      |    | Left | Down | Up   | Right|      |  F12  |
+ * |      | Home |      | Del  |PgDn  |      |    | Left | Down | Up   | Right|      |       |
  * |------+------+------+------+------+------|    |------+------+------+------+------+-------|
  * |      |      |      |      |      | PgUp |    |      |      | ^z[  |      |      |       |
+ * |------+------+------+------+------+------|    |------+------+------+------+------+-------|
+ * |      |      |CURSOR|      |      |      |    |CURSOR|      |      |      |      |       |
  * `------+------+------+------+------+------'    `------+------+------+------+------+-------'
  */
 [_CURSOR] = KEYMAP( \
-  KC_NO  ,KC_F1,  KC_F2,  KC_F3,  KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
   KC_NO  ,KC_NO  ,KC_NO  ,KC_END, KC_NO  , KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_BSPC, \
-  KC_NO  ,KC_HOME,KC_NO  ,KC_DEL, KC_PGDN, KC_NO  ,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_NO  , KC_F12, \
-  KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  , KC_PGUP,  KC_NO  , KC_NO  , KC_NO   , KC_NO  , KC_NO  , KC_NO   \
+  KC_NO  ,KC_HOME,KC_NO  ,KC_DEL, KC_PGDN, KC_NO  ,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_NO  , KC_NO, \
+  KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  , KC_PGUP,  KC_NO  , KC_NO  , M(0)   , KC_NO  , KC_NO  , KC_NO,  \
+  KC_NO  ,KC_NO  ,KC_TRNS,KC_NO  ,KC_NO  , KC_NO  ,  KC_TRNS, KC_NO  , KC_NO   , KC_NO  , KC_NO  , KC_NO
 ),
 
 /* MEDIA
  * ,-----------------------------------------.    ,------------------------------------------.
- * |      |      |      |      |      |      |    |      |      |      |      |      |       |
- * |------+------+------+------+------+------|    |------+------+------+------+------+-------|
  * |      |      |      | MsUp |      |      |    |      |      |      |      | Play |       |
  * |------+------+------+------+------+------|    |------+------+------+------+------+-------|
- * |      |      |MsLeft|MsDown|MsRght|      |    | Prev |VolDn |VolUp | Next |      |       |
+ * |      |      |MsLeft|MsDown|MsRght|      |    | Prev |VolDn |VolUp | Next |      | MEDIA |
  * |------+------+------+------+------+------|    |------+------+------+------+------+-------|
  * |      |      |      |      |      |      |    |      |VolDn_|VolUp_| Mute |      |       |
+ * |------+------+------+------+------+------|    |------+------+------+------+------+-------|
+ * |      |      |      |      |      |      |    |      |      |      |      |      |       |
  * `------+------+------+------+------+------'    `------+------+------+------+------+-------'
  */
 [_MEDIA] = KEYMAP( \
-  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , \
-  KC_NO  , KC_NO  , KC_NO  , KC_MS_U, KC_NO  , KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_MPLY, KC_NO  , \
-  KC_NO  , KC_NO  , KC_MS_L, KC_MS_D, KC_MS_R, KC_NO  ,  KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO  , KC_NO  , \
-  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_NO  , S(LALT(KC_VOLD)), S(LALT(KC_VOLU)), KC_MUTE, KC_NO  , KC_NO   \
+  KC_NO  , KC_NO  , KC_NO  , KC_MS_U, KC_NO  , KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_MPLY, KC_NO, \
+  KC_NO  , KC_NO  , KC_MS_L, KC_MS_D, KC_MS_R, KC_NO  ,  KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO  , KC_TRNS  , \
+  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_NO  , S(LALT(KC_VOLD)), S(LALT(KC_VOLU)), KC_MUTE, KC_NO  , KC_NO,  \
+  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO 
 ),
 
 /* Lower
@@ -95,14 +95,14 @@ CTL_T(KC_ESC),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_
  * |------+------+------+------+------+------|     |------+------+------+------+------+------|
  * |      |      |      |      |      |      |     |  "   |  +   |  <   |   >  |  ?   |   _  |
  * |------+------+------+------+------+------|     |------+------+------+------+------+------|
- * |RESET |      |      |      |      |      |     |      |      |      |      |      |      |
+ * |RESET |      |      |      |LOWER |      |     |      |      |      |      |      |      |
  * `------+------+------+------+------+------'     `------+------+------+------+------+------'
  */
 [_LOWER] = KEYMAP( \
   KC_NO  , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,  KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_ASTR, KC_TRNS,
   KC_NO  , KC_TILD, KC_PIPE, KC_EQL , KC_BSLS, KC_GRV,   KC_QUOT, KC_LCBR, KC_LBRC, KC_RBRC, S(KC_SCLN), KC_RCBR,
   KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_DQT , KC_PLUS, S(KC_COMM),S(KC_DOT),S(KC_SLSH),S(KC_MINS),
-  RESET  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO
+  RESET  , KC_NO  , KC_NO  , KC_NO  , KC_TRNS, KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO
 ),
 
 /* Raise
@@ -131,7 +131,7 @@ CTL_T(KC_ESC),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      | F12  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
+ * |  Fn  |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] =  KEYMAP( \
